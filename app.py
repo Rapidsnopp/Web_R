042fafe9ee0332486e15baaa1534e61b172e398f
Rapidsnopp <160310213+Rapidsnopp@users.noreply.github.com>
2025-02-27 23:52:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
-import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    return "Hello world test"
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=False , host='0.0.0.0' )
+    app.run(debug=False, host='0.0.0.0')
+
